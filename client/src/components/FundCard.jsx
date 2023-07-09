ee2697d9365ced66bb8c7da6b5bf2aa5256ecd56
soumikbaksi18 <baksisoumik@gmail.com>
2023-07-10 01:36:46 +0530

    Cards fixed

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styles from "../styles";
 import { tagType, thirdweb } from "../assets";
 import { daysLeft } from "../utils";
 
@@ -29,7 +29,7 @@ const FundCard = ({
           </div>
 
           <div className="">
-            <h3 className="">{title}</h3>
+            <h3 className={`${styles.texts1}`}>{title}</h3>
             <p className="">{description}</p>
           </div>
 
@@ -55,19 +55,43 @@ const FundCard = ({
         </div>
       </div> */}
 
-      <div className="card w-96 bg-white rounded-md">
+      <div className="card w-96 crowdcard rounded-md">
         <figure>
         <img src={image} alt="fund" className="w-96 h-60 rounded-md " />
         </figure>
         <div className="card-body">
+
           <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
+            <div className="bg-lime-600 m-3 w-28 h-10 flex justify-center items-center rounded-md">
+              <h3 className="m-3 text-white">
+              Afforestation
+              </h3>
+              </div>
           </h2>
-          <p className="">{description}</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+          
+          <div className="">
+            <h3 className={`${styles.texts2} px-4`}>{title}</h3>
+            <p className="">{description}</p>
+          </div>
+
+          <div className="">
+            <div className="">
+              <h4 className="">{amountCollected}</h4>
+              <p className="">Raised of {target}</p>
+            </div>
+            <div className="">
+              <h4 className="">{remainingDays}</h4>
+              <p className="">Days Left</p>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
+              <img alt="user" className="w-1/2 h-1/2 object-contain" />
+            </div>
+            <p className="">
+              by <span className="">{owner}</span>
+            </p>
           </div>
         </div>
       </div>
