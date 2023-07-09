61226c5f0818bf117406908417eb24fe0bcf7c9c
soumikbaksi18 <baksisoumik@gmail.com>
2023-07-10 02:12:36 +0530

    Crowdfunding fixed

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../styles";
 import { tagType, thirdweb } from "../assets";
 import { daysLeft } from "../utils";
+import clogo from "../assets/crowdlogo.svg";
 
 const FundCard = ({
   owner,
@@ -70,29 +71,32 @@ const FundCard = ({
           </h2>
           
           <div className="">
-            <h3 className={`${styles.texts2} px-4`}>{title}</h3>
-            <p className="">{description}</p>
+            <h3 className={`${styles.texts2} px-4 font-lg`}>{title}</h3>
+            <p className="px-4 text-white">{description}</p>
           </div>
 
-          <div className="">
-            <div className="">
-              <h4 className="">{amountCollected}</h4>
-              <p className="">Raised of {target}</p>
-            </div>
-            <div className="">
-              <h4 className="">{remainingDays}</h4>
-              <p className="">Days Left</p>
-            </div>
-          </div>
+          <div className="user flex ">
 
-          <div className="">
-            <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
-              <img alt="user" className="w-1/2 h-1/2 object-contain" />
+            <div className=" rounded-full flex justify-center items-center bg-[#13131a]">
+              <img src={clogo} alt="user" className="w-1/2 h-1/2 object-contain" />
             </div>
             <p className="">
-              by <span className="">{owner}</span>
+              by <span className="text-ellipsis text-white">{owner}</span>
             </p>
           </div>
+
+          <div className="flex justify-between mx-4 my-6">
+            <div className="">
+              <h4 className={`${styles.texts2} px-4 font-lg`}>{amountCollected}</h4>
+              <p className={`${styles.texts2} px-4 font-lg`}>Raised of {target}</p>
+            </div>
+            <div className="">
+              <h4 className={`${styles.texts2} px-4 font-lg`}>{remainingDays}</h4>
+              <p className={`${styles.texts3} px-4 font-lg`}>Days Left</p>
+            </div>
+          </div>
+
+          
         </div>
       </div>
     </>
--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,7 @@
 const styles = {
   texts1: " text-black font-semibold text-lg",
-  texts2: " text-white font-semibold text-lg"
+  texts2: " text-white font-semibold text-lg",
+  texts3: "text-gray-400 font-semibold text-lg ",
 };
 
 export default styles;
